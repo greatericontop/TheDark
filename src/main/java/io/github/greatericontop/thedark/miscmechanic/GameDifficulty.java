2c0c4a74c2b96cd453b65bca9ffe4d33dd186f3d
greatericontop <greateric@protonmail.com>
2025-11-29 16:10:46 -0500

    difficulty enum

---
+++ src/main/java/io/github/greatericontop/thedark/miscmechanic/GameDifficulty.java
@@ -0,0 +1,21 @@
+package io.github.greatericontop.thedark.miscmechanic;
+
+public enum GameDifficulty {
+    IRON(0.9, 28.0, true),
+    GOLD(1.0, 24.0, true),
+    DIAMOND(1.05, 20.0, true),
+    NETHERITE(1.05, 20.0, false),
+
+    ;
+
+    private final double costMultiplier;
+    private final double playerMaxHealth;
+    private final boolean naturalRegeneration;
+
+    GameDifficulty(double costMultiplier, double playerMaxHealth, boolean naturalRegeneration) {
+        this.costMultiplier = costMultiplier;
+        this.playerMaxHealth = playerMaxHealth;
+        this.naturalRegeneration = naturalRegeneration;
+    }
+
+}
